66b408cd965d510e0144a70776d007259552564c
Дмитрий Надобко <dimanadko@mbp-dmitrij.test>
2018-12-18 19:23:51 +0200

    feat(ui): implement WelcomeScreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,13 +1,86 @@
 import React, {Component} from 'react';
-import { Text } from 'react-native';
+import { Container, Text } from 'native-base';
+import { StyleSheet, Image, View } from 'react-native';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
 class WelcomeScreen extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myText: 'I\'m ready to get swiped!',
+    };
+  }
+  onSwipeUp = () => {
+    this.setState({myText: 'You swiped up!'});
+  }
+
+  render() {
         return (
-            <Text>WelcomeScreen</Text>
+          <GestureRecognizer
+            style={{flex: 1}}
+            onSwipeUp={this.onSwipeUp}
+          >
+            <Container>
+
+                <View style={styles.container}>
+                  <View style={styles.logoContainer}>
+                    <Image
+                      source={require('../icons/sloth.png')}
+                      style={styles.logo}
+                    />
+                  </View>
+                  <View style={styles.welcomeContainer}>
+                    <Text style={styles.welcome}>
+                      Welcome to Sloth App
+                    </Text>
+                  </View>
+                  <View style={styles.instructionsContainer}>
+                    <Text style={styles.instructions}>
+                      {this.state.myText}
+                    </Text>
+                  </View>
+                </View>
+            </Container>
+         </GestureRecognizer>
     )
         ;
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcomeContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructionsContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+  },
+  logo: {
+  },
+  logoContainer: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
 
 export default WelcomeScreen;
