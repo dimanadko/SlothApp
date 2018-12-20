21ef2ea7625f118af73d7d771131f67778ea93c1
Дмитрий Надобко <dimanadko@mbp-dmitrij.test>
2018-12-20 14:08:32 +0200

    feat: provide data to scheduleScreen

--- src/screens/ScheduleScreen.js
+++ src/screens/ScheduleScreen.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
 import { Text, Title } from 'native-base';
+import { connect } from 'react-redux'
 
 
 class ScheduleScreen extends Component {
     render() {
-        const data = this.props.data || 'No Data';
+        const { schedules } = this.props;
+        // const data = this.props.data || 'No Data';
+        const title = this.props.title || 'No title';
+        const scheduleData = schedules.find( el => el.name === title);
         return (
             <View>
-                <Text>{data}</Text>
+                <Text>{scheduleData.description}</Text>
+                <Text>{scheduleData.startDate}</Text>
+                <Text>{scheduleData.endDate}</Text>
             </View>
     )
         ;
     }
 }
 
-export default ScheduleScreen;
+export default connect(state => ({schedules: state.manageSchedules.schedules}), () => ({}))(ScheduleScreen);
